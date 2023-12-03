b0ea5eeb0b460537f782e9fa102c2120acf7108f
John Pieri <jvpieri@gmail.com>
2023-12-03 14:12:35 -0400

    Added props to card component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
 import "./App.css";
 import Navbar from "./components/navbar.jsx";
-import Hero from "./components/hero.jsx"
+import Hero from "./components/hero.jsx";
+import Card from "./components/card.jsx";
+import data from "./data.js";
 
 function App() {
+
+  const cards = data.map(card => {
+    return (
+    <Card
+    key={card.id}
+    coverImg={card.coverImg}
+    rating={card.stats.rating}
+    reviewCount={card.stats.reviewCount}
+    location={card.location}
+    title={card.title}
+    price={card.price}
+    />
+    )
+  })
+
   return (
     <>
       <Navbar />
-      <Hero/>
+      <Hero />
+      <section className="card-container">
+      {cards}
+      </section>
     </>
   );
 }
